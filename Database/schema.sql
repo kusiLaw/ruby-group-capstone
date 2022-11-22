98a9d222db7cf69bf91a9ff6c22fbbcb04fe44a5
Krishna <libraryismyparadise@gmail.com>
2022-11-22 20:59:12 +0545

    Add Create book table in schema.sql

--- Database/schema.sql
+++ Database/schema.sql
@@ -11,7 +11,26 @@ CREATE TABLE movie (
   author_id INT FOREIGN KEY REFERENCES authors(id),
   source_id INT FOREIGN KEY REFERENCES sources(id),
   label_id INT FOREIGN KEY REFERENCES labels(id),
-  date DATE NOT NULL,
+  publish_date DATE NOT NULL,
   archived BOOLEAN,
   silent BOOLEAN,
 ) 
+
+/* Create book Table */
+
+
+CREATE TABLE books (
+  id BIGSERIAL NOT NULL PRIMARY KEY,
+  genre_id INT,
+  author_id INT,
+  source_id INT,
+  label_id INT,
+  publish_date DATE NOT NULL,
+  archived BOOLEAN,
+  publisher VARCHAR(100),
+  cover_state VARCHAR(150),
+  FOREIGN KEY (author_id) REFERENCES author(id),
+  FOREIGN KEY (genre_id) REFERENCES genre(id),
+  FOREIGN KEY (label_id) REFERENCES label(id),
+  FOREIGN KEY (source_id) REFERENCES source(id)
+)
