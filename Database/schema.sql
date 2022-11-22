79b01ee74b237e28a54c75e4c21470b745b0133c
Krishna <libraryismyparadise@gmail.com>
2022-11-22 20:53:05 +0545

    Add create movie Table in schema.sql

--- Database/schema.sql
+++ Database/schema.sql
@@ -3,3 +3,15 @@ CREATE TABLE source (
   id BIGSERIAL NOT NULL PRIMARY KEY,
   name VARCHAR(250)
 )
+
+/* Create movie Table */
+CREATE TABLE movie (
+  id BIGSERIAL NOT NULL PRIMARY KEY,
+  genre_id INT FOREIGN KEY REFERENCES genres(id),
+  author_id INT FOREIGN KEY REFERENCES authors(id),
+  source_id INT FOREIGN KEY REFERENCES sources(id),
+  label_id INT FOREIGN KEY REFERENCES labels(id),
+  date DATE NOT NULL,
+  archived BOOLEAN,
+  silent BOOLEAN,
+) 
