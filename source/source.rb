f896caadef4913400557f18969e14c365461479f
John <kibirigejohn005@gmail.com>
2022-11-22 15:02:37 +0300

    fix linter errors

--- source/source.rb
+++ source/source.rb
@@ -1,15 +1,14 @@
-class Source 
-    attr_accessor :name
-    attr_reader :items, :id
+class Source
+  attr_accessor :name
+  attr_reader :items, :id
 
-    def initialize(name)
-        @name = name
-        @id = Random.rand(400...1000)
-    end
+  def initialize(name)
+    @name = name
+    @id = Random.rand(400...1000)
+  end
 
-    def add_item(item)
-        @items << item
-        item.source = self unless item.source == self
-    end
-
+  def add_item(item)
+    @items << item
+    item.source = self unless item.source == self
+  end
 end
