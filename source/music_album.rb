e330605c87ed8da3ebca21c7264461c83b0ccc20
Krishna <libraryismyparadise@gmail.com>
2022-11-22 22:21:33 +0545

    Add MusicAlbum class

---
+++ source/music_album.rb
@@ -0,0 +1,16 @@
+require '../source/item'
+class MusicAlbum < Item
+  attr_accessor :title, :on_spotify
+
+  def initialize(title, on_spotify)
+    super(title)
+    @on_spotify = on_spotify
+    @archived = false
+  end
+
+  private
+
+  def can_be_archived?
+    @archived = super && @on_spotify 
+   end
+end
