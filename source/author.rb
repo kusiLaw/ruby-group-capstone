51f31cc796c599b13459708d02aa7730a6ad7392
kusi <lawcubegsm@gmail.com>
2022-11-22 09:48:10 +0000

    Add class 'author'

---
+++ source/author.rb
@@ -0,0 +1,17 @@
+require './source/item'
+
+class Author
+  attr_accessor :first_name, :last_name, :items
+  attr_reader :id
+
+  def initialize(first_name, last_name)
+    @id = Random.rand(1..1000)
+    @first_name = first_name
+    @last_name = last_name
+    @items = []
+  end
+
+  def add_item(item)
+    @items.push(item) if item.is_a? Item
+  end
+end
