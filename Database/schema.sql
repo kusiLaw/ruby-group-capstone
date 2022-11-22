7b50242df35c746be133578a0b938c31b90189e5
Krishna <libraryismyparadise@gmail.com>
2022-11-22 22:16:20 +0545

    Add create music_album table in schema.sql

--- Database/schema.sql
+++ Database/schema.sql
@@ -34,3 +34,15 @@ CREATE TABLE books (
   FOREIGN KEY (label_id) REFERENCES label(id),
   FOREIGN KEY (source_id) REFERENCES source(id)
 )
+/* Create Music Album Table */
+
+CREATE TABLE music_album (
+  id BIGSERIAL NOT NULL PRIMARY KEY,
+  genre_id INT FOREIGN KEY REFERENCES genres(id),
+  author_id INT FOREIGN KEY REFERENCES authors(id),
+  source_id INT FOREIGN KEY REFERENCES sources(id),
+  label_id INT FOREIGN KEY REFERENCES labels(id),
+  publish_date DATE NOT NULL,
+  archived BOOLEAN,
+  on_spotify BOOLEAN,
+) 
