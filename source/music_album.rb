975101a8bd252ae8d31e9f6145cfb26ebae5c0cd
Krishna <libraryismyparadise@gmail.com>
2022-11-22 22:36:41 +0545

    Add archived instance in music_album.rb

--- source/music_album.rb
+++ source/music_album.rb
@@ -1,6 +1,6 @@
 require '../source/item'
 class MusicAlbum < Item
-  attr_accessor :title, :on_spotify
+  attr_accessor :title, :on_spotify, :archived
 
   def initialize(title, on_spotify)
     super(title)
