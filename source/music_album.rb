abf52b5e419d0fd761b38f6b9276e92ec97d6d54
Krishna <libraryismyparadise@gmail.com>
2022-11-23 14:24:55 +0545

    Modify can_be archived

--- source/music_album.rb
+++ source/music_album.rb
@@ -5,12 +5,11 @@ class MusicAlbum < Item
   def initialize(date, on_spotify)
     super(date)
     @on_spotify = on_spotify
-    @archived = false
   end
 
   private
 
   def can_be_archived?
-    @archived = super && @on_spotify = true
+     super() && @on_spotify
   end
 end
