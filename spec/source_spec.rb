8488cbaa4cca5ed0f68bf43432384e61b97dc9a8
John <kibirigejohn005@gmail.com>
2022-11-25 17:29:40 +0300

    fix linter errors

--- spec/source_spec.rb
+++ spec/source_spec.rb
@@ -1,16 +1,16 @@
-require './source/source.rb'
-require './source/game.rb'
+require './source/source'
+require './source/game'
 
 source = Source.new('Amazon')
 game = Game.new('12/12/2020', true, '12/12/2022')
 
-describe 'Source ' do 
-    it "a source instance is created " do 
-        expect(source).to be_a Source
-    end
+describe 'Source ' do
+  it 'a source instance is created ' do
+    expect(source).to be_a Source
+  end
 
-    it 'the add_item method for the source class' do 
-        source.add_item(game)
-        expect(source.items.length).to be(1)
-    end 
+  it 'the add_item method for the source class' do
+    source.add_item(game)
+    expect(source.items.length).to be(1)
+  end
 end
