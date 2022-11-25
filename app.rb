f839973d8936c024a0c15acabbca766218a00448
John <kibirigejohn005@gmail.com>
2022-11-25 12:07:48 +0300

    list all the music albums created

--- app.rb
+++ app.rb
@@ -9,6 +9,7 @@ require './source/source'
 require './source/label'
 require './source/genre'
 require './source/movie'
+require './source/music_album'
 
 class App
   include List
@@ -26,6 +27,7 @@ class App
   def entry(choice) # rubocop:disable Metrics CyclomaticComplexity
     case choice
     when '1' then list_all_books
+    when '2' then list_all_music_albums
     when '3' then list_all_movies
     when '4' then list_all_games
     when '5' then list_all_genres
@@ -33,6 +35,7 @@ class App
     when '7' then list_all_author
     when '8' then list_all_sources
     when '9' then create_book
+    when '10' then create_music_album
     when '11' then create_movie
     when '12' then create_game
     end
