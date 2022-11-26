27092ce57745e4ea3e6698da2996c03024b9b58f
John <kibirigejohn005@gmail.com>
2022-11-26 04:01:27 +0300

    add readme file

--- main.rb
+++ main.rb
@@ -2,17 +2,20 @@ require './app'
 
 def main # rubocop:disable Metrics/MethodLength
   app = App.new
+  puts ''
+  puts 'Welcome to the Catalog of things APP'
+  puts ''
   until app.terminate
     puts 'Please choose an option by entering a number:
-    1 - List all books
-    2 - List all music albums
-    3 - List all movies
-    4 - List of games
-    5 - List all genres
-    6 - List all labels
-    7 - List all authors
-    8 - List all sources
-    9 - Add a book
+    1  - List all books
+    2  - List all music albums
+    3  - List all movies
+    4  - List of games
+    5  - List all genres
+    6  - List all labels
+    7  - List all authors
+    8  - List all sources
+    9  - Add a book
     10 - Add a music album
     11 - Add a movie
     12 - Add a game
